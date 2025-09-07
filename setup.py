da29a1e5a9bb3a9bcb9ba0fe5a2f313afb51818a
wu <wu@.com>
2025-09-07 10:14:18 +0800

    run tenable/pytenable/gpt-4o-7

--- setup.py
+++ setup.py
@@ -1,37 +1,57 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import os
 
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
+with open('tenable/version.py', 'r') as vfile:
+    exec(vfile.read())
+
+try:
+    long_description = open(
+        os.path.join(
+            os.path.abspath(os.path.dirname(__file__)),
+            'README.rst')).read()
+except:
+    long_description = 'Please refer to https://pytenable.readthedocs.io'
+    print('! could not read README.rst file.')
 
 setup(
-    name='linkedin-jobs-scraper',
-    version='1.4.0',
-    author='Ludovico Fabbri',
-    author_email='ludovico.fabbri@gmail.com',
-    description='Scrape public available jobs on Linkedin using headless browser',
+    name='pyTenable',
+    version=version,
+    description='Python library to interface into Tenable\'s products and applications',
+    author='Tenable, Inc.',
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/spinlud/py-linkedin-jobs-scraper.git',
-    packages=[
-        'linkedin_jobs_scraper',
-        'linkedin_jobs_scraper.chrome_cdp',
-        'linkedin_jobs_scraper.events',
-        'linkedin_jobs_scraper.exceptions',
-        'linkedin_jobs_scraper.filters',
-        'linkedin_jobs_scraper.query',
-        'linkedin_jobs_scraper.strategies',
-        'linkedin_jobs_scraper.utils',
-    ],
-    install_requires=[
-        'selenium',
-        'websocket-client'
-    ],
+    author_email='smcgrath@tenable.com',
+    url='https://github.com/tenable/pytenable',
+    license='MIT',
     classifiers=[
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
-    python_requires='>=3.6',
+    keywords='tenable tenable_io securitycenter containersecurity',
+    packages=find_packages(exclude=['docs', 'tests']),
+    install_requires=[
+        'requests>=2.19',
+        'python-dateutil>=2.6',
+        'semver>=2.8.1',
+        'ipaddress>=1.0.22',
+        'restfly>=1.3.5',
+        'marshmallow>=3.6',
+        'python-box>=4.0',
+    ],
+    extras_require={
+        'NessusReportv2': ['defusedxml>=0.5.0'],
+        'PWCertAuth': ['requests-pkcs12>=1.3'],
+        'docker': ['docker>=3.7.2'],
+        'complete': [
+            'defusedxml>=0.5.0',
+            'requests-pkcs12>=1.3',
+            'docker>=3.7.2',
+        ]
+    }
 )
